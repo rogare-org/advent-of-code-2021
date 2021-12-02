0a40f5d138aafa8eb09ae4dc73a396697d6ba9b5
Vree, P <pascal.vree@rogare.org>
2021-12-02 17:01:57 +0100

    implemented a straightforward solution to the advent of code 2021 challenges for day 1 & 2

---
+++ day1/solution-to-challenges.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const reduceToMeasurementTuplesOfLength3 = (carry, currentItem, currentIndex, source) => {
+	if (currentItem !== undefined && source[currentIndex + 1] !== undefined && source[currentIndex + 2] !== undefined) {
+		carry.push(currentItem + source[currentIndex + 1] + source[currentIndex + 2]);
+	}
+
+	return carry;
+};
+
+const reduceToNrOfTimesMeasurementIncreases = (carry, currentItem, currentIndex, source) => {
+	if (currentItem !== undefined && source[currentIndex + 1] !== undefined) {
+		if (currentItem < source[currentIndex + 1]) {
+			return carry + 1;
+		}
+	}
+
+	return carry;
+}
+
+const mapToMeasurement = (parsedMeasurement, measurements) => measurements.push(Number.parseInt(parsedMeasurement, 10));
+
+const path = require('path');
+require('../shared/csv-data-loader.js')(path.resolve(__dirname, 'data.csv'), mapToMeasurement)
+	.then(measurements => {
+		console.log('day 1, challenge 1: ', measurements.reduce(reduceToNrOfTimesMeasurementIncreases, 0));
+		console.log('day 1, challenge 2: ', measurements.reduce(reduceToMeasurementTuplesOfLength3, []).reduce(reduceToNrOfTimesMeasurementIncreases, 0));
+	});
+
---
+++ day2/solution-to-challenges.js
@@ -0,0 +1,66 @@
+'use strict';
+
+/*
+ * xpr $(cat data.csv | grep -i forward | awk '{ print $2" +" } END { print "0" }') => 1817
+ * xpr $(cat data.csv | grep -i down | awk '{ print $2" +" } END { print "0" }')  => 2034
+ * xpr $(cat data.csv | grep -i up | awk '{ print $2" +" } END { print "0" }') => 962
+ */
+
+const mapToDiveInstruction = (parsedDiveInstruction, diveInstructions) => diveInstructions.push({ direction: parsedDiveInstruction[0], distance: Number.parseInt(parsedDiveInstruction[1], 10) });
+
+const path = require('path');
+require('../shared/csv-data-loader.js')(path.resolve(__dirname, 'data.csv'), mapToDiveInstruction)
+	.then(diveInstructions => {
+
+		const startingPosition = () => ({ horizontalPosition: 0, depth: 0 });
+
+		const applyDiveInstruction = (divePosition, diveInstruction) => {
+
+			if (diveInstruction.direction.startsWith('forward')) {
+				divePosition.horizontalPosition += diveInstruction.distance;
+			}
+
+			if (diveInstruction.direction.startsWith('down')) {
+				divePosition.depth += diveInstruction.distance;
+			}
+
+			if (diveInstruction.direction.startsWith('up')) {
+				divePosition.depth -= diveInstruction.distance;
+			}
+
+			return divePosition;
+		}
+
+		const finalDivePosition = diveInstructions.reduce(applyDiveInstruction, startingPosition());
+		console.log('day 2, challenge 1:', finalDivePosition.horizontalPosition * finalDivePosition.depth);
+		return diveInstructions;
+	})
+	.then(diveInstructions => {
+
+		const startingPosition = () => ({ horizontalPosition: 0, depth: 0, aim: 0 });
+
+		const applyDiveInstruction = (divePosition, diveInstruction) => {
+
+			if (diveInstruction.direction.startsWith('forward')) {
+				divePosition.horizontalPosition += diveInstruction.distance;
+				divePosition.depth += divePosition.aim * diveInstruction.distance;
+			}
+
+			if (diveInstruction.direction.startsWith('down')) {
+				divePosition.aim += diveInstruction.distance;
+			}
+
+			if (diveInstruction.direction.startsWith('up')) {
+				divePosition.aim -= diveInstruction.distance;
+			}
+
+			return divePosition;
+		}
+
+
+		const finalDivePosition = diveInstructions.reduce(applyDiveInstruction, startingPosition());
+		console.log('day 2, challenge 2:', finalDivePosition.horizontalPosition * finalDivePosition.depth);
+	});
+;
+
+
