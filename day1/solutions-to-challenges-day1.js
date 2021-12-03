f4715b1eb8a926f420f4ecbe32505d0f092c13ff
Vree, P <pascal.vree@rogare.org>
2021-12-03 18:07:00 +0100

    refactor(day1, all solutions): added comments, updated code layout to be conform coding style, simplified logic, updated names to better reflect their purpose

--- day1/solutions-to-challenges-day1.js
+++ day1/solutions-to-challenges-day1.js
@@ -1,32 +1,36 @@
 'use strict';
 
-const reduceToMeasurementTuplesOfLength3 = (carry, currentItem, currentIndex, source) => {
-  if (
-    currentItem !== undefined &&
-    source[currentIndex + 1] !== undefined &&
-    source[currentIndex + 2] !== undefined
-  ) {
-    carry.push(currentItem + source[currentIndex + 1] + source[currentIndex + 2]);
-  }
+const reduceToEveryTripleMeasurementsAsSummation = (carry, currentItem, currentIndex, source) => {
+  // when reduce is called with an initialValue, the initial currentIndex will be 0 otherwise it will be 1
+  if (currentIndex === 0 || (currentIndex === 1 && typeof carry !== 'number'))
+    throw new TypeError("Dont provide 'reduce' any initialValue, make sure it reduces an Array of Numbers");
 
-  return carry;
+  const stillBeforeThirdMeasurement = currentIndex <= 1; // Array Indexes start at 0
+  if (stillBeforeThirdMeasurement) return [];
+
+  // select previous 2 entries and sum with the current entry
+  const aggregation = source[currentIndex - 2] + source[currentIndex - 1] + source[currentIndex];
+  return carry.concat(aggregation);
 };
 
-const reduceToNrOfTimesMeasurementIncreases = (carry, currentItem, currentIndex, source) => {
-  if (currentItem !== undefined && source[currentIndex + 1] !== undefined) {
-    if (currentItem < source[currentIndex + 1]) {
-      return carry + 1;
-    }
-  }
+const reduceToNrOfTimesEveryPairIncreases = (carry, currentItem, currentIndex, source) => {
+  // when reduce is called with an initialValue, the initial currentIndex will be 0 otherwise it will be 1
+  if (currentIndex === 0 || (currentIndex === 1 && typeof carry !== 'number'))
+    throw new TypeError("Dont provide 'reduce' any initialValue, make sure it reduces an Array of Numbers");
+
+  const stillBeforeOrAtSecondMeasurement = currentIndex <= 1; // Array Indexes start at 0
+  if (stillBeforeOrAtSecondMeasurement) carry = 0;
+
+  const previousItem = source[currentIndex - 1];
+  const downwardTrend = currentItem === Math.max(currentItem, previousItem);
+  if (downwardTrend) carry += 1;
 
   return carry;
 };
 
 module.exports = () => ({
-  solutionToChallenge1Day1: (measurements) =>
-    measurements.reduce(reduceToNrOfTimesMeasurementIncreases, 0),
+  solutionToChallenge1Day1: (measurements) => measurements.reduce(reduceToNrOfTimesEveryPairIncreases),
+
   solutionToChallenge2Day1: (measurements) =>
-    measurements
-      .reduce(reduceToMeasurementTuplesOfLength3, [])
-      .reduce(reduceToNrOfTimesMeasurementIncreases, 0),
+    measurements.reduce(reduceToEveryTripleMeasurementsAsSummation).reduce(reduceToNrOfTimesEveryPairIncreases),
 });
